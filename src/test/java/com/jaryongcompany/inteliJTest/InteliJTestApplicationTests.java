f14e3e98607789e4c9c2c7de71dc490427389744
chojuyong <jaryonge@hanmail.net>
2022-12-16 16:39:38 +0900

    인텔리제이

---
+++ src/test/java/com/jaryongcompany/inteliJTest/InteliJTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaryongcompany.inteliJTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteliJTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
